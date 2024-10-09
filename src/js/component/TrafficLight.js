3a9511bb3b9d153f9ec08e8adf31fd0e574ff567
lf14 <54648106+lf14@users.noreply.github.com>
2024-10-09 18:32:07 +0000

    [Revisión 1]
    TrafficLight.js-se modifica el parametro que recibe
    set color para que solo cambie al hacer click en
    un color y no sigiendo el patrón.

--- src/js/component/TrafficLight.js
+++ src/js/component/TrafficLight.js
@@ -1,24 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const TrafficLight = () => {
-  const [color, setColor] = useState("null");
+  const [color, setColor] = useState(null);
 
-  const colorOnClick = () => {
-    if (color === "red") {
-      setColor("yellow");
-    } else if (color === "yellow") {
-      setColor("green");
-    } else {
-      setColor("red");
-    }
+  const colorOnClick = (selectedColor) => {
+    setColor(selectedColor);
   };
 
   return (
     <div id="trafficBracket">
       <div id="container">
-        <div className={color === "red" ? "red light selected" : "red light"} onClick={colorOnClick}></div>
-        <div className={color === "yellow" ? "yellow light selected" : "yellow light"} onClick={colorOnClick}></div>
-        <div className={color === "green" ? "green light selected" : "green light"} onClick={colorOnClick}></div>
+        <div className={color === "red" ? "red light selected" : "red light"} onClick={() => colorOnClick("red")}></div>
+        <div className={color === "yellow" ? "yellow light selected" : "yellow light"} onClick={() => colorOnClick("yellow")}></div>
+        <div className={color === "green" ? "green light selected" : "green light"} onClick={() => colorOnClick("green")}></div>
       </div>
     </div>
   );
